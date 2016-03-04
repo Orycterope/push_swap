6882bb58c8e35aee70ae0a5ba49c2e4acfb5ccdc
Thomas VERMEILH <tvermeil@student.42.fr>
2016-03-04 15:18:04 +0100

    writes push operation

--- src/solver.c
+++ src/solver.c
@@ -6,7 +6,7 @@
 /*   By: tvermeil <tvermeil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/13 17:40:12 by tvermeil          #+#    #+#             */
-/*   Updated: 2016/03/04 14:50:29 by tvermeil         ###   ########.fr       */
+/*   Updated: 2016/03/04 15:06:41 by tvermeil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,9 @@ int		get_sorted_amount_of_pile(t_pile *pile, int sens)
 		if ((sens == 1 && get_rank_of(pile->next) == get_rank_of(pile) - 1)
 			|| (sens == -1 && get_rank_of(pile->next) == get_rank_of(pile) + 1))
 			out += get_rank_of(pile) * get_rank_of(pile->next) * 10;
-		/*if ((sens == 1 && get_rank_of(pile) == size && get_rank_of(pile->previous) == 1)
+		if ((sens == 1 && get_rank_of(pile) == size && get_rank_of(pile->previous) == 1)
 			|| (sens == -1 && get_rank_of(pile) == size && get_rank_of(pile->next) == 1))
-			out += size; */
+			out += size * 10;
 		rank -= sens;
 		pile = pile->next;
 	}
@@ -61,34 +61,35 @@ t_pile	**choose_op(t_pile *pile_tab[], char **op_lst, char *flags)
 	int			current_value; // not so useful
 	int			i;
 
-	//printf("----------- begin sa:\n");
-	//print_piles(pile_tab); //
+	printf("----------- begin sa:\n");
+	print_piles(pile_tab); //
 	pile_tab = do_operation("sa", pile_tab, NULL, NULL);
-//	print_piles(pile_tab); //
+	print_piles(pile_tab); //
 	best_op = "sa";
-	best_value = get_sorted_amount(pile_tab);
-	//printf("[sa] -> %d\n", best_value);
+	best_value = get_sorted_amount(pile_tab); //
+	printf("[sa] -> %d\n", best_value);
 	pile_tab = do_operation("sa", pile_tab, NULL, NULL);
-//	print_piles(pile_tab); //
-//	printf("----------- end sa:\n");
+	print_piles(pile_tab); //
+	printf("----------- end sa:\n"); //
 
 	i = -1;
 	while (operations[++i] != NULL)
 	{
-//		printf("----------- begin %s:\n", operations[i]);
-		if (!(pile_tab[1] == NULL && (ft_strstr("sb pa ss rb rrb rrr rr", operations[i]))))
+		printf("----------- begin %s:\n", operations[i]); //
+		if (!(get_pile_size(pile_tab[1]) < 2
+					&& (ft_strstr("sb pa ss rb rrb rrr rr", operations[i]))))
 		{
-//			print_piles(pile_tab); //
+			print_piles(pile_tab); //
 			pile_tab = do_operation(operations[i], pile_tab, NULL, NULL);
-//			print_piles(pile_tab); //
+			print_piles(pile_tab); //
 			current_value = get_sorted_amount(pile_tab);
 			best_op = (current_value > best_value) ? operations[i] : best_op;
 			best_value = (current_value > best_value) ? current_value : best_value;
-//			printf("[%s] -> %d\n", operations[i], current_value);
+			printf("[%s] -> %d\n", operations[i], current_value); //
 			pile_tab = do_operation(reverse_op[i], pile_tab, NULL, NULL);
-//			print_piles(pile_tab); //
+			print_piles(pile_tab); //
 		}
-//		printf("----------- end %s:\n", operations[i]);
+		printf("----------- end %s:\n", operations[i]); //
 	}
 	pile_tab = do_operation((char *)best_op, pile_tab, op_lst, flags);
 	return (pile_tab);
